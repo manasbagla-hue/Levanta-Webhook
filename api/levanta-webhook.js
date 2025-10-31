8bdfbff2eac90a5b0a6cebd6a04df6c5f49d65dd
manasbagla-hue <manas.bagla@vcommission.com>
2025-10-31 10:09:11 +0530

    Update levanta-webhook.js

--- api/levanta-webhook.js
+++ api/levanta-webhook.js
@@ -1,1 +1,9 @@
+import { Resend } from 'resend';
+const resend = new Resend(process.env.RESEND_API_KEY);
 
+await resend.emails.send({
+  from: 'webhook@yourdomain.com',
+  to: 'manasbagla@vcommission.com',
+  subject: `Levanta: ${event.type}`,
+  text: message
+});
